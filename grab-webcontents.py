5ddea0464a63c22a606f9411a0688d585cd272a0
GeorgeWildboarman <georgepinetree555@gmail.com>
2020-06-22 16:12:25 +0900

    Modified grab-webcontents.py

--- grab-webcontents.py
+++ grab-webcontents.py
@@ -1,20 +1,24 @@
+#! usr/bin/python3
 '''
 
-Script Analysis of 'Friends', popular American television sitcom.
+Grab the Scripts of 'Friends' from the resource website.
 
-All the resources of the scripts used here are provided by Crazy For Friends.   
+All the resources of the scripts are provided by Crazy For Friends.   
     Resource website URL: http://www.livesinabox.com/friends/scripts.shtml
 
-First step: Get the contents of the scripts divided into episodes on each page. 
+The resources are divided into each episode.
+
+usage: grab-webcontents.py [directory name]
+
 
 '''
 
+import sys
 import re
 import os
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-
 
 # Assign Header information of your User-Agent.
 headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 9; Lenovo TB-7305F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Safari/537.36'}
@@ -28,24 +32,25 @@ resp = requests.get(url, timeout=60, headers=headers)
 # Modify the encoding of the response if you need.
 resp.encoding = resp.apparent_encoding
 
-# Ppass the respsonse to BeautifulSoup.
+# Pass the respsonse to BeautifulSoup.
 soup = BeautifulSoup(resp.text,'html.parser')
 
-# Find out 'a' tags to.
+# Find out 'a' tags.
 atags = soup.find_all('a')
 
-# Assign a re pattern to list links of episodes.
+# Assign a Regex pattern to list links of episodes.
 pattern = r'Episode\s*\d+'
 
-# List links of episodes.
+# List links of the episodes.
 links = [urljoin(url,lnk.get('href')) for lnk in atags 
         if re.search(pattern,lnk.get_text(strip=True))]
 
-#for l in links: print(l)
-#print(len(links))
-
-# Make dir to store all the script contents.
-dr = '.scripts'
+# Create a directory to store all the script contents in it.
+if len(sys.argv)>1:
+    dr = sys.argv[1]
+else:
+    dr = '.scripts'
+print('Created Directory: ',dr)
 os.makedirs(dr, exist_ok=True)
 
 # Assign file names by each of the episodes.
@@ -54,7 +59,7 @@ paths = [os.path.join(dr,os.path.basename(p)) for p in links]
 #for p in paths: print(p)
 #print(len(paths))
 
-# Store the contents in local files by each of the episodes.
+# Write the contents in local files by each of the episodes.
 #print(links[0])
 #print(paths[0])
 #for i in range(1):
